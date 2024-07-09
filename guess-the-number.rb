c3ccbe273a282cd282e9f1265141164f10395523
Devin de Spindler <devin.vipxxx@gmail.com>
2024-07-09 22:20:05 +0200

    feat: Guess the number game changes colour based on closeness of guess The "Your guess was too high/low" return changes colour if the user is closer, at 3, 2 and 1 number close to the correct number.

--- guess-the-number.rb
+++ guess-the-number.rb
@@ -5,17 +5,47 @@ class String
     def green          
         "\e[32m#{self}\e[0m" 
     end
+    def red
+        "\e[31m#{self}\e[0m"
+    end
+    def orange
+        "\e[38;5;214m#{self}\e[0m"
+    end
+    def yellow
+        "\e[33m#{self}\e[0m"
+    end
 end
 
 while numberToGuess != userGuess
     puts "Type a number between 1 and 20"
     userGuess = gets.chomp.to_i
 
+    numberTooHigh = userGuess - numberToGuess
     if numberToGuess < userGuess
-        puts "Your guess was too high"
-        end 
+        case numberTooHigh
+        when 3
+            puts "Your guess was too high".red
+        when 2
+            puts "Your guess was too high".orange
+        when 1
+            puts "Your guess was too high".yellow
+        else
+            puts "Your guess was too high"
+        end
+    end 
+
+    numberTooLow = numberToGuess - userGuess
     if numberToGuess > userGuess
-        puts "Your guess was too low"
-    end
+        case numberTooLow
+        when 3
+            puts "Your guess was too low".red
+        when 2
+            puts "Your guess was too low".orange
+        when 1
+            puts "Your guess was too low".yellow
+        else
+            puts "Your guess was too low"
+        end
+    end 
 end
 puts "Correct, the number was #{numberToGuess}".green
